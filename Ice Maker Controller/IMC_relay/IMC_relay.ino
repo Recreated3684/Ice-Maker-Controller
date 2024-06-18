1fcd3b36fbbf6e18b9a9195ceeadf28c6e7e57da
Recreated3684 <DStewart3684@gmail.com>
2024-06-18 13:09:19 -0400

    Update IMC_relay.ino
    Increase the button press time from 500ms to 3000ms to prevent missed trigger signals.

--- Ice Maker Controller/IMC_relay/IMC_relay.ino
+++ Ice Maker Controller/IMC_relay/IMC_relay.ino
@@ -126,7 +126,7 @@ void printCurrentTime() {
 void triggerRelay() {
   Serial.println("Relay triggered");
   digitalWrite(relayPin, LOW); // Set GPIO pin low to activate relay
-  delay(500); // Keep the relay on for 500 ms
+  delay(3000); // Keep the relay on for 500 ms
   digitalWrite(relayPin, HIGH); // Set GPIO pin high to deactivate relay
 
   // SOS pattern: dot-dot-dot, dash-dash-dash, dot-dot-dot
